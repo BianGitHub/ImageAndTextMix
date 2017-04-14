94116f5f4f213e99d82d38ba4fe3b59590d30075
边雷 <775884216@qq.com>
2017-04-14 10:11:25 +0800

    项目准备

---
+++ ImageAndTextMixBtn/ImageAndTextMixBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageAndTextMixBtn
+//
+//  Created by 边雷 on 17/4/14.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageAndTextMixBtn/ImageAndTextMixBtn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ImageAndTextMixBtn
+//
+//  Created by 边雷 on 17/4/14.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"图文混排";
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
