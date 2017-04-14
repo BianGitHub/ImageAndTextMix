c06531bea541d30ba3c7f955ded364ffcc9d4e49
边雷 <775884216@qq.com>
2017-04-14 10:49:49 +0800

    Btn图文混排

--- ImageAndTextMixBtn/ImageAndTextMixBtn/TopView.m
+++ ImageAndTextMixBtn/ImageAndTextMixBtn/TopView.m
@@ -7,6 +7,8 @@
 //
 
 #import "TopView.h"
+#import "NSAttributedString+Addition.h"
+#import "Masonry.h"
 
 @implementation TopView
 
@@ -19,6 +21,56 @@
 
 - (void)setupUI {
     self.backgroundColor = [UIColor blackColor];
+    
+    [self addButtons:@"image1" title:@"彩票"];
+    [self addButtons:@"image2" title:@"加油"];
+    [self addButtons:@"image3" title:@"吃饭"];
+    [self addButtons:@"image4" title:@"货运"];
+}
+
+- (void)addButtons:(NSString *)imageName title:(NSString *)title {
+    UIButton *btn = [[UIButton alloc] init];
+    
+    NSAttributedString *str = [NSAttributedString bl_imageTextWithImage:[UIImage imageNamed:imageName] imageWH:35 title:title fontSize:14 titleColor:[UIColor whiteColor] spacing:7];
+    [btn setAttributedTitle:str forState:UIControlStateNormal];
+    
+    // 自动换行
+    btn.titleLabel.numberOfLines = 0;
+    btn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [btn sizeToFit];
+    
+    [self addSubview:btn];
+}
+
+/*
+ 1.控件创建的时候([alloc init])，不会调用这个方法
+ 2.布局子控件的时候，它会自动调到这个方法。
+ 3.子控件的位置或者大小发生变化的时候，也会调用这个方法
+ 4.父控件的位置或者大小发生变化的时候，也会调用这个方法。
+ 5.如果应用程序支持多屏幕朝向，当屏幕发生旋转的时候，也会调用这个方法。
+ */
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    CGFloat margin = (self.frame.size.width - self.subviews[0].frame.size.width * 4) / 5;
+    
+    // 需要减一, 不然数组越界
+    for (NSInteger i = 0; i < self.subviews.count - 1; i++) {
+        UIButton *btn = self.subviews[i];
+        UIButton *nextBtn = self.subviews[i+1];
+        
+        if (i == 0) {
+            [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+                make.left.equalTo(self).offset(margin);
+                make.centerY.equalTo(self);
+            }];
+        }
+        
+        [nextBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.equalTo(btn.mas_right).offset(margin);
+            make.centerY.equalTo(self);
+        }];
+    }
 }
 
 @end
